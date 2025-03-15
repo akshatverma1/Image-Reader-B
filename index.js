cd996e7004c4ea528177af3aa8e5d2c8deae8160
akshatverma1 <akshaverma14@gmail.com>
2025-03-15 16:21:29 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const fs = require("fs");
+app.listen(port,()=>{
+    console.log("Server Is Running On Port = "+port);
+})
+
+app.get("/",(req,res)=>{
+    res.send("Hello My Name is Akku");    
+})
+
+// Corrected import
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Initialize with your API key
+const genAI = new GoogleGenerativeAI("AIzaSyDwp56xlfDqoRMm6Cs9GSp9hjvq7UyKn-w");
+
+// Example usage
+// async function run() {
+//     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+//     const prompt = "Does this image look like,give anwer in more than 200 words";
+//     // const image = {
+//     //     inlineData: {
+//     //         data: Buffer.from(fs.readFileSync("1.png")).toString("base64"),
+//     //         mimeType: "image/png",
+//     //     },
+//     const image = "https://i.pinimg.com/736x/c3/89/72/c389727463b0bb82a68cf739a79494fc.jpg";
+
+//     const result = await model.generateContent([prompt,image]);
+//     console.log("answer"+" "+"="+" "+result.response.text());
+
+// }
+
+// run();
+
+
+app.get("/data/:id",(req,res)=>{
+    let {id} = req.params;
+    console.log(id);
+    async function run() {
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    
+        const prompt = "Does this image look like,give anwer in more than 200 words";
+        // const image = {
+        //     inlineData: {
+        //         data: Buffer.from(fs.readFileSync("1.png")).toString("base64"),
+        //         mimeType: "image/png",
+        //     },
+        const image = id;
+    
+        const result = await model.generateContent([prompt,image]);
+        console.log("answer"+" "+"="+" "+result.response.text());
+    
+    }
+})
